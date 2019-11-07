56ae5142caf7eb7d046d9a6f7585b9d49fb77131
TanguyDH <tanguydeherdt@hotmail.com>
2019-11-07 11:58:52 +0100

    upd

--- src/components/SelectLanguage/SelectLanguage.js
+++ src/components/SelectLanguage/SelectLanguage.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import './SelectLanguage.scss';
+import ReactFlagsSelect from 'react-flags-select';
+import 'react-flags-select/css/react-flags-select.css';
 
 export default () => {
   return (
-    <div>
-      oulou
-    </div>
+      <div className="SelectLanguage">
+      <ReactFlagsSelect
+          defaultCountry="FR"
+          countries={["GB", "FR", 'NL']}
+          customLabels={{ "GB": "EN", "FR": "FR", "NL": 'NL' }}
+          showSelectedLabel={false} />
+      </div>
   )
 }
+
+
+
+
+
