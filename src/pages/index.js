0887a978381c7728e420f4d1b55345f4b395ae95
TanguyDH <tanguydeherdt@hotmail.com>
2021-06-23 11:42:55 +0200

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,11 +9,13 @@
 import ContactCircle from '../components/ContactCircle/ContactCircle';
   import Vison from '../components/Vision/Vision';
 import WhyUs from '../components/WhyUs/WhyUs';
+import { Helmet } from 'react-helmet' 
 
   export default class Index extends Component {
     render() {
       return (
         <Layout bg={true} blue={true}>
+        <Helmet title="Water Hill, votre outil de communication" defer={false} />
           <MainBackground />
           <Poducts />
           <Vison />
